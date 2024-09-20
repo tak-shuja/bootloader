d28bf33971185db44d1a792754e67befd61958d3
Shuja Ahmad <shujatak@protonmail.com>
2024-09-20 07:14:26 -0400

    Added some helpful debug messages

--- utils/data.asm
+++ utils/data.asm
@@ -1,7 +1,7 @@
 ; DATA SECTION
 hello_msg: db "Bootloader Program Initialization Complete!", 0x0a, 0x0d, 0x00
 
-read_disk_success_msg: db "It 'b' is printed next, the program works", 0x0a,0x0d, 0x00
+read_disk_success_msg: db "If 'b' is printed next, the program works", 0x0a,0x0d, 0x00
 
 debug: db "DEBUG MESSAGE", 0x0a, 0x0d, 0x00
 
